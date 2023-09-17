34803e9897676e8c09c21663630d0985b307fddf
Amresh Maurya <amresh.terminal@gmail.com>
2023-09-17 16:30:54 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import createError from 'http-errors';
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.send({ name: 'Amresh', message: 'it Works bro!' });
+});
+
+//middleware
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        logger.error(err.message);
+
+        const statusCode = 500;
+
+        res.status(statusCode).json({
+            errors: [
+                {
+                    type: err.name,
+                    msg: err.message,
+                    path: '',
+                    location: '',
+                },
+            ],
+        });
+    }
 });
 
 export default app;
